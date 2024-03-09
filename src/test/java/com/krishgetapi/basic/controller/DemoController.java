e0bbd7272b06d9d8896f127055e6365522e0652a
bhowmickkrishnendu <krishnendu2u@gmail.com>
2024-03-09 09:13:53 +0530

    ⚡RestTemplate added to fetch HTTP response

--- src/test/java/com/krishgetapi/basic/controller/DemoController.java
+++ src/test/java/com/krishgetapi/basic/controller/DemoController.java
@@ -1,11 +1,16 @@
 package com.krishgetapi.basic.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class DemoController {
 
+    @Autowired
+    private RestTemplate restTemplate;
     @GetMapping("/")
     public String getRootMessage() {
         return "ROOT API";
@@ -15,6 +20,10 @@ public class DemoController {
     public String getCheckResponseMessage() {
         return "It's Perfectly Working!";
     }
-    
-    
+
+    @GetMapping("/checkInternet")
+    public ResponseEntity<byte[]> getCheckInternet() {
+        ResponseEntity<byte[]> response = restTemplate.getForEntity("https://media.tenor.com/o2rlJc1PoLMAAAAi/dancin-monkey.gif", byte[].class);
+    return response;
+    }
 }
