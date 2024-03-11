4ab8b9f4add2c6d6ee2d5c967a4636f1edce22e8
bhowmickkrishnendu <krishnendu2u@gmail.com>
2024-03-11 08:45:33 +0530

    ✅ Post mapping method added for login

--- src/test/java/com/krishgetapi/basic/controller/DemoController.java
+++ src/test/java/com/krishgetapi/basic/controller/DemoController.java
@@ -1,8 +1,13 @@
 package com.krishgetapi.basic.controller;
 
+// import com.krishgetapi.basic.model.User;
+import com.krishgetapi.basic.model.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -11,6 +16,10 @@ public class DemoController {
 
     @Autowired
     private RestTemplate restTemplate;
+
+    @Autowired
+    private UserRepository userRepository;
+
     @GetMapping("/")
     public String getRootMessage() {
         return "ROOT API";
@@ -25,5 +34,15 @@ public class DemoController {
     public ResponseEntity<byte[]> getCheckInternet() {
         ResponseEntity<byte[]> response = restTemplate.getForEntity("https://media.tenor.com/o2rlJc1PoLMAAAAi/dancin-monkey.gif", byte[].class);
     return response;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody User user) {
+        User existingUser = userRepository.findByUsernameAndPassword(user.getUsername(), user.getPassword());
+        if (existingUser != null) {
+            return ResponseEntity.ok("Login successful. Welcome " + existingUser.getFirstname() + " " + existingUser.getLastname());
+        } else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid credentials");
+        }
     }
 }
