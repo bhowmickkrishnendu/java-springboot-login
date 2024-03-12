160e537880e4afe66f47885749f1dc79c797b09a
bhowmickkrishnendu <krishnendu2u@gmail.com>
2024-03-12 08:50:07 +0530

    ✅ User repository added

---
+++ src/test/java/com/krishgetapi/basic/model/UserRepository.java
@@ -0,0 +1,7 @@
+package com.krishgetapi.basic.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsernameAndPassword(String username, String password);
+}
